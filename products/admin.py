ec93ee2dfc603758ebdda8004df967b18da17e70
JaeWong91 <Jaewong19@hotmail.co.uk>
2021-04-24 15:09:32 +0000

    customized admin

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,26 @@ from .models import Product, Category
 
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)  # to reverse it, just put " -'sku' "
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)  # register our new classes with their respective models
+admin.site.register(Category, CategoryAdmin)
