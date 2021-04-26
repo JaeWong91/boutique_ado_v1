c9edde5a88deec8285af34602f414e1faf7a99ba
JaeWong91 <Jaewong19@hotmail.co.uk>
2021-04-26 16:11:18 +0000

    added JS and back to top button

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Category
 
 # Create your views here.
@@ -21,7 +23,7 @@ def all_products(request):
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
-                products = product.annotate(lower_name=Lower('name'))
+                products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'  # the double underscore allows us to drill into a related model
             if 'direction' in request.GET:
@@ -29,7 +31,6 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'  # the minus means to reverse the order
             products = products.order_by(sortkey)
-
 
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
